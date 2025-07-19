fd8208f0f947d4a70a4870adc8a6f510caeeb049
kashpr619 <alwahhadkash@gmail.com>
2025-07-19 10:40:39 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,80 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-closed-view",
+      "name": "closed view",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5206919846943627,
+          "pitch": -0.052240242069064635,
+          "rotation": 0,
+          "target": "1-open-view"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-open-view",
+      "name": "open view",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
